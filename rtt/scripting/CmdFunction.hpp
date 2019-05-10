8a7c19ca93547c486c90734418d1ea0c67eee6cb
Johannes Meyer <johannes@intermodalics.eu>
2019-05-10 22:46:10 +0200

    Partially revert "scripting: Fixed collecting from a SendHandle returned from sending an exported function"
    This reverts commit 361fe2909aaeb81b7cee83600b44c62e5b22aa7a.
    The changes in unit tests have been preserved.

--- rtt/scripting/CmdFunction.hpp
+++ rtt/scripting/CmdFunction.hpp
@@ -48,7 +48,7 @@ namespace RTT
                       )
         : minit(init_com),
         mrunner(p), mcaller(caller),
-        _foo( foo ), ss(SendFailure), isqueued(false), maccept(false)
+        _foo( foo ), ss(SendNotReady), isqueued(false), maccept(false)
         {
         }
 
@@ -62,7 +62,6 @@ namespace RTT
                 // this is asyn behaviour :
                 if (isqueued == false ) {
                     isqueued = true;
-                    ss = SendNotReady;
                     // is called before runFunction is executed.
                     minit->readArguments();
                     maccept = minit->execute() && mrunner->runFunction( _foo.get() );
@@ -83,7 +82,7 @@ namespace RTT
             } catch (...) {
                 cout << "CmdFunction threw an exception" <<endl;
             }
-            return ss;
+            return ss = SendNotReady;
         }
 
         virtual SendStatus value() const {
@@ -103,7 +102,7 @@ namespace RTT
             if (_foo->isLoaded()) mrunner->removeFunction( _foo.get() );
             maccept = false;
             isqueued = false;
-            ss = SendFailure;
+            ss = SendNotReady;
         }
 
         CmdFunction* clone() const
