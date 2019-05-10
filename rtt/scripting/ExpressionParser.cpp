483028ff68d711b151a451a03a60f96faa8728e1
Johannes Meyer <johannes@intermodalics.eu>
2019-05-10 22:47:16 +0200

    Revert "Revert "scripting: fix .cmd() in while loops or state programs""
    This reverts commit f82fa9a3e596675c25bb37ecae76abfed25a7839.

--- rtt/scripting/ExpressionParser.cpp
+++ rtt/scripting/ExpressionParser.cpp
@@ -118,7 +118,7 @@ namespace RTT
     {
       std::string name( begin, end );
       mobject = name.substr(0, name.length() - 1);
-    }
+    };
 
   void DataCallParser::seenmethodname( iter_t begin, iter_t end )
     {
@@ -140,7 +140,7 @@ namespace RTT
           mmethod = name;
       }
 //      cout << "seenmethodname "<< mobject << "." << mmethod<<endl;
-    }
+    };
 
   void DataCallParser::seendataname()
   {
@@ -231,7 +231,7 @@ namespace RTT
                         args.insert( args.begin(), sha->getDataSource() );
                         if (meth == "collect")
                             ret = sha->getFactory()->produceCollect(args, new ValueDataSource<bool>(true) );// blocking
-                        else // (meth == "collectIfDone")
+                        else
                             ret = sha->getFactory()->produceCollect(args, new ValueDataSource<bool>(false) );// non-blocking
                         return;
                     }
@@ -291,7 +291,7 @@ namespace RTT
     {
       delete argparsers.top();
       argparsers.pop();
-    }
+    };
   }
 
   ConstructorParser::ConstructorParser( ExpressionParser& p, CommonParser& cp)
@@ -313,7 +313,7 @@ namespace RTT
     {
       delete argparsers.top();
       argparsers.pop();
-    }
+    };
   }
 
 
@@ -528,7 +528,7 @@ namespace RTT
 //       ( str_p( "s" ) | "ms" | "us" | "ns" )[
 //         bind( &ExpressionParser::seentimeunit, this, _1, _2 ) ] ) | expression[bind(&ExpressionParser::seentimeexpr, this)];
 
-  }
+  };
 
     void ExpressionParser::inverttime()
     {
@@ -568,7 +568,7 @@ namespace RTT
     default:
         std::string arg(begin, end);
         throw parse_exception_semantic_error("Expected time expression 's', 'ms', 'us' or 'ns' after integer value, got "+arg);
-    }
+    };
 
     parsestack.push( new ConstantDataSource<double>( total ) );
 
